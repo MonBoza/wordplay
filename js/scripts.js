0c5ef2ce66c9cbfe0ef5d67e13e1ebe864a23aca
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-24 20:18:46 -0500

    reversed input results

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,19 @@ function sentenceInput (event){
     event.preventDefault();
     const input = document.getElementById("input").value;
     const result = document.getElementById("result");
-    const sentenceArray = input.split("");
+    const sentenceArray = input.split(" ");
     //result.innerText = sentenceArray
     const newWordsArray = [];
     sentenceArray.forEach(function(word){
-    if (word.length >= 3){
-        newWordsArray.push(word);
-        result.innerText = newWordsArray.join(",");
+       if (word.length >= 3){
+         newWordsArray.push(word);
+         //result.innerText = newWordsArray.join(" ");
+    const reversedWords = [];
+    for (let i = sentenceArray.length-1; i >=0; i--){
+        const valueAtIndex = sentenceArray[i]
+        reversedWords.push(valueAtIndex);
+        result.innerText = reversedWords
+    }
 
     }
    
